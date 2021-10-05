2f806989b4ba74d44916eff5bd2ea4c05a4330dc
Matteo <matteo.tuzi00@gmail.com>
2021-10-05 22:09:02 +0200

    Inizializzato il progetto

---
+++ cmake-build-debug/CMakeFiles/progettoSo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/progettoSo.dir/main.c.obj"
+  "libprogettoSo.dll.a"
+  "progettoSo.exe"
+  "progettoSo.exe.manifest"
+  "progettoSo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/progettoSo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
