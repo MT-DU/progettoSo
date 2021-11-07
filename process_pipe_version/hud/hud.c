88c1c8576e3933cbcf0127ba47cbe09318e23af9
Barux <davide.u01@gmail.com>
2021-11-07 23:05:25 +0100

    updated makefile and hud

--- process_pipe_version/hud/hud.c
+++ process_pipe_version/hud/hud.c
@@ -11,29 +11,26 @@ void setupColors(){
  * @brief Crea la finestra di dialogo
  * 
  */
-void createWindow(){
+void mainWindow(){
     WINDOW* hud_win = newwin(0,0,0,0); // dovrebbe essere fullscreen
     const int MAX_Y = getmaxy(hud_win), MAX_X = getmaxx(hud_win);
     wattron(hud_win, COLOR_PAIR(1));
     //wcolor_set(hud_win, COLOR_BLACK, NULL);
     //wbkgd(hud_win, COLOR_YELLOW);
     char scelta1[] = "Play the game.";
-    mvwprintw(hud_win, 0, 5,                                                             
-                    "     \t\t  _     ___           _     \n"        
-" ___ ___ ___ ___ ___    _| |___|  _|___ ___ _| |___ ___ ___ \n"
-"|_ -| . | .'|  _| -_|  | . | -_|  _| -_|   | . | -_|  _|_ -|\n"
-"|___|  _|__,|___|___|  |___|___|_| |___|_|_|___|___|_| |___|\n"
-    "|_|\n"                                                     );
+    char logo[1000] = "";
+    createLogo(logo);
+    mvwprintw(hud_win, 0, 0, logo);
+    mvwprintw(hud_win, 10, 0, "%d %d", MAX_X, MAX_Y);
     wmove(hud_win, 5,0);
     whline(hud_win, ACS_HLINE, getmaxx(hud_win));
     wattron(hud_win, COLOR_PAIR(3));
     wbkgd(hud_win, COLOR_RED);
 
-    mvwprintw(hud_win, (MAX_Y/2) - sizeof(scelta1), (MAX_X/2) - sizeof(scelta1), scelta1);
+    mvwprintw(hud_win, MAX_Y/2, MAX_X/2, "%s", scelta1);
     refresh();
     wgetch(hud_win);
-    // gnome-terminal --geometry=widthxheight+Xposition+Yposition
-    // resize -s <rows> <columns>
+
 }
 
 /**
@@ -46,8 +43,20 @@ void mainApp(){
     setupColors();
     noecho();
     curs_set(NO_CURSOR);
-    createWindow();
+    mainWindow();
     getch();
     refresh();
     endwin();
+}
+
+void createLogo (char sus[300]) {
+
+    strcat(sus,LOGO1);
+    strcat(sus,"\n");
+    strcat(sus,LOGO2);
+    strcat(sus,"\n");
+    strcat(sus,LOGO3);
+    strcat(sus,"\n");
+    strcat(sus,LOGO4);
+
 }
--- process_pipe_version/hud/hud.h
+++ process_pipe_version/hud/hud.h
@@ -3,24 +3,21 @@
 #include <sys/wait.h>
 #include <ncurses.h>
 #include "../errors/errors.h"
+#include <string.h>
 
 #define NO_CURSOR 0
 #define YES_CURSOR 1
 #define STARSHIP {'\n',' ',' ','/','\\','\n','>','|',' ',' ','|','>','\n',' ',' ','\\','/','\n','\n'}
-#define LOGO {}
-// potrebbe servire mettere un ' ' all'inizio in caso non si cancelli
+#define LOGO1 " ___ ___ ___ ___ ___    _| |___|  _|___ ___ _| |___ ___ ___ "
+#define LOGO2 "|_ -| . | .'|  _| -_|  | . | -_|  _| -_|   | . | -_|  _|_ -|"
+#define LOGO3 "|___|  _|__,|___|___|  |___|___|_| |___|_|_|___|___|_| |___|"
+#define LOGO4 "    |_|                                                     "
+
 void setupColors();
 void processStarShip();
-void createWindow();
+void mainWindow();
 void mainApp();
+void createLogo(char[300]);
 
-
-/*
-
- ___ ___ ___ ___ ___    _| |___|  _|___ ___ _| |___ ___ ___ 
-|_ -| . | .'|  _| -_|  | . | -_|  _| -_|   | . | -_|  _|_ -|
-|___|  _|__,|___|___|  |___|___|_| |___|_|_|___|___|_| |___|
-    |_|
-
-*/
-
+// gnome-terminal --geometry=widthxheight+Xposition+Yposition
+// resize -s <rows> <columns>
