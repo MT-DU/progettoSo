00e58f15e4e3d527a81a401acfc0e3d12dc630c1
MatteoTuzi <matteo.tuzi00@gmail.com>
2021-11-07 23:54:31 +0100

    Implementazione delle voci di menu

--- process_pipe_version/hud/hud.c
+++ process_pipe_version/hud/hud.c
@@ -1,5 +1,66 @@
 #include "hud.h"
 
+int divideByTwo(int coord){
+    return coord/2;
+}
+
+//TODO Da rivedere la logica dello switch, ha piu' utilita' per creare il processo del gioco
+void printMainMenu(WINDOW *win, int max_y, int max_x, int selector){
+    switch(selector){
+        case PLAY_GAME_NUMBER:
+            mvwprintw(win, divideByTwo(max_y), divideByTwo(max_x) - divideByTwo(sizeof(PLAY_GAME)), PLAY_GAME);
+            break;
+        case MODE_GAME_NUMBER:
+            mvwprintw(win, divideByTwo(max_y) + SPACE_BETWEEN_CHOICES, divideByTwo(max_x) - divideByTwo(sizeof(MODE_GAME)), MODE_GAME);
+            break;
+        case QUIT_GAME_NUMBER:
+            mvwprintw(win, divideByTwo(max_y) + SPACE_BETWEEN_CHOICES*selector, divideByTwo(max_x) - divideByTwo(sizeof(QUIT_GAME)), QUIT_GAME);
+            break;
+    }
+}
+
+int keyPadSelector(WINDOW *win, int min_y, int max_y, int counter){
+    cbreak();
+    keypad(win, true);
+    int key;
+    key = wgetch(win);
+    switch (key){
+        case KEY_UP:
+            if(counter>min_y){
+                counter--;
+            }
+            break;
+        case KEY_DOWN:
+            if(counter<max_y){
+                counter++;
+            }
+    }
+    nocbreak();
+    return counter;
+}
+
+/**
+ * @brief Stampa il logo del gioco
+ * 
+ * @param win 
+ * @param max_x 
+ */
+void printLogo (WINDOW* win, int max_x) {
+    int halfLenLogo = 30;
+    char logo1[sizeof(LOGO2)] = "", logo2[sizeof(LOGO2)] = "", logo3[sizeof(LOGO3)] = "", logo4[sizeof(LOGO4)] = "";
+
+    strcat(logo1, LOGO1);
+    strcat(logo2, LOGO2);
+    strcat(logo3, LOGO3);
+    strcat(logo4, LOGO4);
+    
+    mvwprintw(win, 1, divideByTwo(max_x) - halfLenLogo, logo1);
+    mvwprintw(win, 2, divideByTwo(max_x) - halfLenLogo, logo2);
+    mvwprintw(win, 3, divideByTwo(max_x) - halfLenLogo, logo3);
+    mvwprintw(win, 4, divideByTwo(max_x) - halfLenLogo, logo4);
+
+}
+
 void setupColors(){
     start_color();
     init_pair(1,COLOR_BLACK,COLOR_YELLOW); 
@@ -14,17 +75,16 @@ void setupColors(){
 void mainWindow(){
     WINDOW* hud_win = newwin(0,0,0,0); // dovrebbe essere fullscreen
     const int MAX_Y = getmaxy(hud_win), MAX_X = getmaxx(hud_win);
+    int i;
     wattron(hud_win, COLOR_PAIR(1));
-    wcolor_set(hud_win, COLOR_BLACK, NULL);
-    //wbkgd(hud_win, COLOR_YELLOW);
-    char scelta1[] = "Play the game.";
     printLogo(hud_win, MAX_X);
     wmove(hud_win, 6,0);
     whline(hud_win, ACS_HLINE, getmaxx(hud_win));
     wattron(hud_win, COLOR_PAIR(3));
-    //wbkgd(hud_win, COLOR_RED);
-
-    mvwprintw(hud_win, MAX_Y/2, MAX_X/2 - strlen(scelta1), "%s", scelta1);
+    //keyPadSelector(hud_win, MAX_Y, );
+    for(i = 0; i < 3; i++){
+        printMainMenu(hud_win, MAX_Y, MAX_X, i);
+    }
     refresh();
     wgetch(hud_win);
 
@@ -47,20 +107,4 @@ void mainApp(){
     refresh();
     getch();
     endwin();
-}
-
-void printLogo (WINDOW* win, int max_x) {
-
-    char logo1[sizeof(LOGO1)] = "", logo2[sizeof(LOGO2)] = "", logo3[sizeof(LOGO3)] = "", logo4[sizeof(LOGO4)] = "";
-
-    strcat(logo1, LOGO1);
-    strcat(logo2, LOGO2);
-    strcat(logo3, LOGO3);
-    strcat(logo4, LOGO4);
-
-    mvwprintw(win, 1, max_x/2 - 30, logo1);
-    mvwprintw(win, 2, max_x/2 - 30, logo2);
-    mvwprintw(win, 3, max_x/2 - 30, logo3);
-    mvwprintw(win, 4, max_x/2 - 30, logo4);
-
 }
--- process_pipe_version/hud/hud.h
+++ process_pipe_version/hud/hud.h
@@ -4,7 +4,7 @@
 #include <ncurses.h>
 #include "../errors/errors.h"
 #include <string.h>
-
+/* */
 #define NO_CURSOR 0
 #define YES_CURSOR 1
 #define STARSHIP {'\n',' ',' ','/','\\','\n','>','|',' ',' ','|','>','\n',' ',' ','\\','/','\n','\n'}
@@ -12,12 +12,23 @@
 #define LOGO2 "|_ -| . | .'|  _| -_|  | . | -_|  _| -_|   | . | -_|  _|_ -|"
 #define LOGO3 "|___|  _|__,|___|___|  |___|___|_| |___|_|_|___|___|_| |___|"
 #define LOGO4 "    |_|                                                     "
+#define SPACE_BETWEEN_CHOICES 3
+#define PLAY_GAME_NUMBER 0
+#define MODE_GAME_NUMBER 1
+#define QUIT_GAME_NUMBER 2
 
+#define PLAY_GAME "Play the game."
+#define MODE_GAME "Select the Game Mode."
+#define QUIT_GAME "Quit the game."
+
+int divideByTwo(int coord);
+void printMainMenu(WINDOW *win, int max_y, int max_x, int selector);
+int keyPadSelector(WINDOW *win, int min_y, int max_y, int counter);
+void printLogo(WINDOW *win, int max_x);
 void setupColors();
 void processStarShip();
 void mainWindow();
 void mainApp();
-void printLogo(WINDOW *win, int max_x);
 
 // gnome-terminal --geometry=widthxheight+Xposition+Yposition
 // resize -s <rows> <columns>
